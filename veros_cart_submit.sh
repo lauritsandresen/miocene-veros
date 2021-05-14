57c1e5f3e27a89bfb92c2929383c27443278ef56
lauritsandresen <82801425+lauritsandresen@users.noreply.github.com>
2021-05-14 09:17:40 +0200

    Create veros_cart_submit.sh

---
+++ veros_cart_submit.sh
@@ -0,0 +1,12 @@
+## This file is an example of a script for submitting veros on cartesius
+#!/bin/bash
+#SBATCH -p short
+#SBATCH -t 0:05:00
+#SBATCH -N 1
+#SBATCH -n 16
+#SBATCH --exclusive
+
+module purge
+module load 2019
+module load Python/3.7.5-foss-2018b HDF5/1.10.2-foss-2018b PETSc/3.11.2-foss-2018b foss/2018b
+srun python -m mpi4py global_one_degree.py -n 8 2
